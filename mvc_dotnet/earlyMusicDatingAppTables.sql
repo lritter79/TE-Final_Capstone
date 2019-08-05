46e435af27944c5f3f8cbe1f0c4fa4a0328610db
Levon Ritter <levong2112@gmail.com>
2019-08-05 18:35:54 -0400

    added tables to database

--- mvc_dotnet/earlyMusicDatingAppTables.sql
+++ mvc_dotnet/earlyMusicDatingAppTables.sql
@@ -25,8 +25,24 @@ home_state varchar(50) NOT NULL,
 self_description varchar(Max) NOT NULL,
 password_hash varchar(50) NOT NULL,
 salt varchar(50) NOT NUll,
-CONSTRAINT Pk_users PRIMARY KEY (ID)
+CONSTRAINT pk_users PRIMARY KEY (ID)
 
+);
+
+CREATE TABLE Places (
+ID int identity (1, 1) NOT NULL,
+city varchar(50) NOT NULL,
+state_name varchar(50) NOT NULL,
+from_date datetime NOT NULL,
+to_date datetime NOT NULL,
+CONSTRAINT fk_places_users FOREIGN KEY (ID) REFERENCES Users (ID)
+);
+
+CREATE TABLE Instruments_Played (
+ID int identity (1, 1) NOT NULL,
+instrument_name varchar(50) NOT NULL,
+
+CONSTRAINT fk_instuments_played_users FOREIGN KEY (ID) REFERENCES Users (ID)
 );
 
 COMMIT
