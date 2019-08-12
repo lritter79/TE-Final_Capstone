a54bfba88c51c263672b63c59c4c60273108f2fb
Christine Stewart <tinestewart@gmail.com>
2019-08-12 16:09:16 -0400

    styled biopage

--- mvc_dotnet/WebApplication.Web/Controllers/AccountController.cs
+++ mvc_dotnet/WebApplication.Web/Controllers/AccountController.cs
@@ -85,12 +85,7 @@ namespace WebApplication.Web.Controllers
                 authProvider.Register(registerViewModel.Email, registerViewModel.Username, registerViewModel.BirthDate, registerViewModel.HomeCity, registerViewModel.HomeState, registerViewModel.SelfDescription, registerViewModel.Password, role: "User"); 
 
                 // Redirect the user where you want them to go after registering
-<<<<<<< HEAD
-                return RedirectToAction("Index", "Home");
-            }
 
-            return View(registerViewModel);
-=======
                 return RedirectToAction("RegistrationComplete", "Account");
             }
 
@@ -101,7 +96,7 @@ namespace WebApplication.Web.Controllers
         public IActionResult RegistrationComplete()
         {
             return View();
->>>>>>> ea482559e5c189e00a8440bbc6973587fe595024
+
         }
 
         [HttpGet]
