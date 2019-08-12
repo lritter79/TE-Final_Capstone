7b51c71e8e0be398d2958670fdb1ad1299b3f203
Levon Ritter <levong2112@gmail.com>
2019-08-12 09:16:09 -0400

    whatever

---
+++ mvc_dotnet/WebApplication.Web/Models/Account/BioUpdateModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Web.Models.Account
+{
+    public class BioUpdateModel
+    {
+
+        [Display(Name = "Self Description")]
+        public string SelfDescription { get; set; }
+
+        
+       
+    }
+}
